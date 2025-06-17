e9ab18c80ca64a62223e1e196cdcd6d466b8c856
Alexandru Iuga <alex.iuga@gmail.com>
2025-06-17 11:24:06 +0300

    updated copyright year

--- sources/CaveOfWonders.Ports.SheetsAccess/ISheets.cs
+++ sources/CaveOfWonders.Ports.SheetsAccess/ISheets.cs
@@ -1,5 +1,5 @@
 ﻿// Cave of Wonders
-// Copyright (C) 2023-2024 Dust in the Wind
+// Copyright (C) 2023-2025 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 
 
 // Cave of Wonders
-// Copyright (C) 2023-2024 Dust in the Wind
+// Copyright (C) 2023-2025 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
