aa47dccb6d87c7acb001f0adf90dca0c464fc9bc
Alexandru Iuga <alex.iuga@gmail.com>
2025-07-13 12:59:09 +0300

    removed the web app

--- pot-viewer/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { PotList } from './components/pot-list/pot-list';
-
-export const routes: Routes = [
-  { path: '', component: PotList },
-  { path: 'pots', component: PotList },
-  { path: '**', redirectTo: '' }
-];
