9d541a4aa9fc74631e8d8177f36f2cafe3bb78e4
Alexandru Iuga <alex.iuga@gmail.com>
2026-06-22 12:37:30 +0300

    Added port for importing Fintown transactions.

---
+++ sources/CaveOfWonders.Adapters.FintownAccess/FintownService.cs
@@ -0,0 +1,41 @@
+﻿using DustInTheWind.CaveOfWonders.DataTypes;
+using DustInTheWind.CaveOfWonders.Domain;
+using DustInTheWind.CaveOfWonders.Ports.FintownAccess;
+using DustInTheWind.Fintown.Toolkit;
+using System.Runtime.CompilerServices;
+
+namespace DustInTheWind.CaveOfWonders.Adapters.FintownAccess;
+
+public class FintownService : IFintownService
+{
+    public async IAsyncEnumerable<Gem> GetGemsAsync(string filePath, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        TransactionsDocument transactionsDocument = await TransactionsDocument.LoadFromFileAsync(filePath);
+
+        IEnumerable<Gem> gems = transactionsDocument.Transactions
+            .Select(CreateGem);
+        
+        foreach (Gem gem in gems)
+            yield return gem;
+    }
+
+    private static Gem CreateGem(TransactionRecord transactionRecord)
+    {
+        Gem gem = new()
+        {
+            Date = transactionRecord.Date,
+            Amount = transactionRecord.Amount.Value
+        };
+
+        if (transactionRecord.Description == "Investing funds")
+            gem.Category = GemCategory.Internal;
+        else if (transactionRecord.Description == "Deposit funds")
+            gem.Category = GemCategory.Deposit;
+        else if (transactionRecord.Description == "Interest Pay-Out")
+            gem.Category = GemCategory.Gain;
+        else
+            gem.Category = GemCategory.Unknown;
+
+        return gem;
+    }
+}
---
+++ sources/CaveOfWonders.Ports.FintownAccess/IFintownService.cs
@@ -0,0 +1,8 @@
+﻿using DustInTheWind.CaveOfWonders.Domain;
+
+namespace DustInTheWind.CaveOfWonders.Ports.FintownAccess;
+
+public interface IFintownService
+{
+    IAsyncEnumerable<Gem> GetGemsAsync(string filePath, CancellationToken cancellationToken);
+}
