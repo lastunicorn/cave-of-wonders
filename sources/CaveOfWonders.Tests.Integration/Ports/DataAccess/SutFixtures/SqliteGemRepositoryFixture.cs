00dd00b73f1d3b986fdeebd57ad96e47e4236238
Alexandru Iuga <alex.iuga@gmail.com>
2026-07-15 19:33:26 +0300

    Fixed the SQLite database directory cleanup.

--- sources/CaveOfWonders.Tests.Integration/Ports/DataAccess/SutFixtures/SqliteGemRepositoryFixture.cs
+++ sources/CaveOfWonders.Tests.Integration/Ports/DataAccess/SutFixtures/SqliteGemRepositoryFixture.cs
@@ -2,23 +2,34 @@ using DustInTheWind.CaveOfWonders.Adapters.DataAccess.SQLite;
 using DustInTheWind.CaveOfWonders.Adapters.DataAccess.SQLite.Repositories;
 using DustInTheWind.CaveOfWonders.Domain;
 using DustInTheWind.CaveOfWonders.Ports.DataAccess;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace DustInTheWind.CaveOfWonders.Tests.Integration.Ports.DataAccess.SutFixtures;
 
 internal class SqliteGemRepositoryFixture : IGemRepositorySutFixture
 {
-	private readonly string dbFilePath = Path.Combine(Path.GetTempPath(), $"test-database-{Guid.NewGuid()}.db");
+	private readonly string dbDirectoryPath = Path.Combine(Path.GetTempPath(), $"test-database-{Guid.NewGuid()}");
+	private readonly string dbFilePath;
+	private readonly string connectionString;
 
 	private CaveOfWondersDbContext dbContext;
 	private IPotRepository potRepository;
 
 	public IGemRepository Instance { get; private set; }
 
+	public SqliteGemRepositoryFixture()
+	{
+		dbFilePath = Path.Combine(dbDirectoryPath, "test-database.db");
+		connectionString = $"Data Source={dbFilePath}";
+	}
+
 	public async Task CreateSutAsync(CancellationToken cancellationToken = default)
 	{
+		Directory.CreateDirectory(dbDirectoryPath);
+
 		DbContextOptions<CaveOfWondersDbContext> options = new DbContextOptionsBuilder<CaveOfWondersDbContext>()
-			.UseSqlite($"Data Source={dbFilePath}")
+			.UseSqlite(connectionString)
 			.Options;
 
 		dbContext = new CaveOfWondersDbContext(options);
@@ -45,9 +56,7 @@ internal class SqliteGemRepositoryFixture : IGemRepositorySutFixture
 
 	public Task ResetAsync(CancellationToken cancellationToken = default)
 	{
-		if (File.Exists(dbFilePath))
-			File.Delete(dbFilePath);
-
+		DeleteDatabaseDirectory();
 		return Task.CompletedTask;
 	}
 
@@ -58,8 +67,18 @@ internal class SqliteGemRepositoryFixture : IGemRepositorySutFixture
 		potRepository = null;
 		Instance = null;
 
-		if (File.Exists(dbFilePath))
-			File.Delete(dbFilePath);
+		DeleteDatabaseDirectory();
+	}
+
+	private void DeleteDatabaseDirectory()
+	{
+		// A pooled connection can keep the .db/-wal/-shm files open behind the scenes even after
+		// Dispose, which would otherwise make the directory deletion below fail intermittently.
+		SqliteConnection sqliteConnection = new(connectionString);
+		SqliteConnection.ClearPool(sqliteConnection);
+
+		if (Directory.Exists(dbDirectoryPath))
+			Directory.Delete(dbDirectoryPath, true);
 	}
 
 	public override string ToString()
--- sources/CaveOfWonders.Tests.Integration/Ports/DataAccess/SutFixtures/SqlitePotRepositoryFixture.cs
+++ sources/CaveOfWonders.Tests.Integration/Ports/DataAccess/SutFixtures/SqlitePotRepositoryFixture.cs
@@ -2,22 +2,33 @@ using DustInTheWind.CaveOfWonders.Adapters.DataAccess.SQLite;
 using DustInTheWind.CaveOfWonders.Adapters.DataAccess.SQLite.Repositories;
 using DustInTheWind.CaveOfWonders.Ports.DataAccess;
 using DustInTheWind.CaveOfWonders.Tests.Utils;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace DustInTheWind.CaveOfWonders.Tests.Integration.Ports.DataAccess.SutFixtures;
 
 internal class SqlitePotRepositoryFixture : ISutFixture<IPotRepository>
 {
-	private readonly string dbFilePath = Path.Combine(Path.GetTempPath(), $"test-database-{Guid.NewGuid()}.db");
+	private readonly string dbDirectoryPath = Path.Combine(Path.GetTempPath(), $"test-database-{Guid.NewGuid()}");
+	private readonly string dbFilePath;
+	private readonly string connectionString;
 
 	private CaveOfWondersDbContext dbContext;
 
 	public IPotRepository Instance { get; private set; }
 
+	public SqlitePotRepositoryFixture()
+	{
+		dbFilePath = Path.Combine(dbDirectoryPath, "test-database.db");
+		connectionString = $"Data Source={dbFilePath}";
+	}
+
 	public async Task CreateSutAsync(CancellationToken cancellationToken = default)
 	{
+		Directory.CreateDirectory(dbDirectoryPath);
+
 		DbContextOptions<CaveOfWondersDbContext> options = new DbContextOptionsBuilder<CaveOfWondersDbContext>()
-			.UseSqlite($"Data Source={dbFilePath}")
+			.UseSqlite(connectionString)
 			.Options;
 
 		dbContext = new CaveOfWondersDbContext(options);
@@ -37,9 +48,7 @@ internal class SqlitePotRepositoryFixture : ISutFixture<IPotRepository>
 
 	public Task ResetAsync(CancellationToken cancellationToken = default)
 	{
-		if (File.Exists(dbFilePath))
-			File.Delete(dbFilePath);
-
+		DeleteDatabaseDirectory();
 		return Task.CompletedTask;
 	}
 
@@ -49,8 +58,18 @@ internal class SqlitePotRepositoryFixture : ISutFixture<IPotRepository>
 		dbContext = null;
 		Instance = null;
 
-		if (File.Exists(dbFilePath))
-			File.Delete(dbFilePath);
+		DeleteDatabaseDirectory();
+	}
+
+	private void DeleteDatabaseDirectory()
+	{
+		// A pooled connection can keep the .db/-wal/-shm files open behind the scenes even after
+		// Dispose, which would otherwise make the directory deletion below fail intermittently.
+		SqliteConnection sqliteConnection = new(connectionString);
+		SqliteConnection.ClearPool(sqliteConnection);
+
+		if (Directory.Exists(dbDirectoryPath))
+			Directory.Delete(dbDirectoryPath, true);
 	}
 
 	public override string ToString()
