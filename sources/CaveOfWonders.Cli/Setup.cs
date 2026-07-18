dff77a1ddf5f889c401845505a3d2a889d16a807
Alexandru Iuga <alex.iuga@gmail.com>
2026-07-18 11:37:21 +0300

    Configured sqlite usage in cli app

--- sources/CaveOfWonders.Cli/Setup.cs
+++ sources/CaveOfWonders.Cli/Setup.cs
@@ -1,6 +1,7 @@
 ﻿using DustInTheWind.CaveOfWonders.Adapters.BnrAccess;
 using DustInTheWind.CaveOfWonders.Adapters.ClockAccess;
 using DustInTheWind.CaveOfWonders.Adapters.DataAccess.Json;
+using DustInTheWind.CaveOfWonders.Adapters.DataAccess.SQLite;
 using DustInTheWind.CaveOfWonders.Adapters.FileAccess;
 using DustInTheWind.CaveOfWonders.Adapters.FintownAccess;
 using DustInTheWind.CaveOfWonders.Adapters.InsAccess;
@@ -17,70 +18,134 @@ using DustInTheWind.CaveOfWonders.Ports.InsAccess;
 using DustInTheWind.CaveOfWonders.Ports.LogAccess;
 using DustInTheWind.CaveOfWonders.Ports.MintosAccess;
 using DustInTheWind.CaveOfWonders.Ports.SpreadsheetAccess;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Data.Common;
+using System.Diagnostics;
+using SQLiteUnitOfWork = DustInTheWind.CaveOfWonders.Adapters.DataAccess.SQLite.UnitOfWork;
+using JsonUnitOfWork = DustInTheWind.CaveOfWonders.Adapters.DataAccess.Json.UnitOfWork;
 
 namespace DustInTheWind.CaveOfWonders.Cli;
 
 internal static class DependenciesSetup
 {
-    public static void Configure(IServiceCollection serviceCollection)
-    {
-        // Register Configuration
-        IConfiguration configuration = new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
-            .Build();
-        
-        serviceCollection.AddSingleton(configuration);
+	public static void Configure(IServiceCollection serviceCollection)
+	{
+		// Register Configuration
+		IConfiguration configuration = new ConfigurationBuilder()
+			.SetBasePath(AppContext.BaseDirectory)
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+			.Build();
 
-        // Register MediatR
-        serviceCollection.AddMediatR(config =>
-            config.RegisterServicesFromAssembly(typeof(PresentPotsRequest).Assembly));
+		serviceCollection.AddSingleton(configuration);
 
-        // Register JSON Database
-        serviceCollection.AddScoped(services =>
-        {
-	        IConfiguration configuration = services.GetRequiredService<IConfiguration>();
-            string connectionString = configuration.GetConnectionString("DefaultConnection");
-            return new Database(connectionString);
-        });
+		// Register MediatR
+		serviceCollection.AddMediatR(config =>
+			config.RegisterServicesFromAssembly(typeof(PresentPotsRequest).Assembly));
 
-        serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+		RegisterSqLiteDatabase(serviceCollection);
+		//RegisterJsonDatabase(serviceCollection);
 
-        serviceCollection.AddSingleton<ISystemClock, SystemClock>();
-        serviceCollection.AddScoped<IBnrService, BnrService>();
-        serviceCollection.AddScoped<IInsService, InsService>();
-        serviceCollection.AddScoped<IMintosService, MintosService>();
-        serviceCollection.AddScoped<IFintownService, FintownService>();
-        serviceCollection.AddScoped<ISheets, Sheets>();
-        serviceCollection.AddScoped<ILog, Log>();
-        serviceCollection.AddSingleton<IFileSystem, FileSystem>();
+		serviceCollection.AddSingleton<ISystemClock, SystemClock>();
+		serviceCollection.AddScoped<IBnrService, BnrService>();
+		serviceCollection.AddScoped<IInsService, InsService>();
+		serviceCollection.AddScoped<IMintosService, MintosService>();
+		serviceCollection.AddScoped<IFintownService, FintownService>();
+		serviceCollection.AddScoped<ISheets, Sheets>();
+		serviceCollection.AddScoped<ILog, Log>();
+		serviceCollection.AddSingleton<IFileSystem, FileSystem>();
 
-        serviceCollection.AddSingleton<ICpiImportExportFactory, CpiImportExportFactory>();
-        serviceCollection.AddScoped<FileCpiImportExport>();
-        serviceCollection.AddScoped<WebCpiImportExport>();
+		serviceCollection.AddSingleton<ICpiImportExportFactory, CpiImportExportFactory>();
+		serviceCollection.AddScoped<FileCpiImportExport>();
+		serviceCollection.AddScoped<WebCpiImportExport>();
 
-        serviceCollection
-            .AddSingleton(context =>
-            {
-                ICpiImportExportFactory cpiImportExportFactory = context.GetRequiredService<ICpiImportExportFactory>();
-                CpiImportExportPool cpiImportExportPool = new(cpiImportExportFactory);
-                
-                cpiImportExportPool.Add(new CpiImportExportDescriptor
-                {
-                    Id = new Guid("bb7590ef-6126-4529-8012-b6a8a4c6f903"),
-                    Type = typeof(FileCpiImportExport)
-                });
-                
-                cpiImportExportPool.Add(new CpiImportExportDescriptor
-                {
-                    Id = new Guid("3ff33b30-a149-4f08-b545-e524fd3e4384"),
-                    Type = typeof(WebCpiImportExport)
-                });
-                
-                return cpiImportExportPool;
-            });
-    }
+		serviceCollection
+			.AddSingleton(context =>
+			{
+				ICpiImportExportFactory cpiImportExportFactory = context.GetRequiredService<ICpiImportExportFactory>();
+				CpiImportExportPool cpiImportExportPool = new(cpiImportExportFactory);
+
+				cpiImportExportPool.Add(new CpiImportExportDescriptor
+				{
+					Id = new Guid("bb7590ef-6126-4529-8012-b6a8a4c6f903"),
+					Type = typeof(FileCpiImportExport)
+				});
+
+				cpiImportExportPool.Add(new CpiImportExportDescriptor
+				{
+					Id = new Guid("3ff33b30-a149-4f08-b545-e524fd3e4384"),
+					Type = typeof(WebCpiImportExport)
+				});
+
+				return cpiImportExportPool;
+			});
+	}
+
+	private static void RegisterJsonDatabase(IServiceCollection serviceCollection)
+	{
+		// Register JSON Database
+		serviceCollection.AddScoped(services =>
+		{
+			return Measure.Action("Creating UnitOfWork", () =>
+			{
+				IConfiguration configuration = services.GetRequiredService<IConfiguration>();
+				string connectionString = configuration.GetConnectionString("DefaultConnection");
+				return new Database(connectionString);
+			});
+		});
+
+		serviceCollection.AddScoped<IUnitOfWork, JsonUnitOfWork>();
+	}
+
+	private static void RegisterSqLiteDatabase(IServiceCollection serviceCollection)
+	{
+		// Register SQLite Database
+		serviceCollection.AddDbContext<CaveOfWondersDbContext>((services, options) =>
+		{
+			IConfiguration configuration = services.GetRequiredService<IConfiguration>();
+			string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+			string dataSource = new SqliteConnectionStringBuilder(connectionString).DataSource;
+			string databaseDirectoryPath = Path.GetDirectoryName(Path.GetFullPath(dataSource, AppContext.BaseDirectory));
+
+			if (!string.IsNullOrEmpty(databaseDirectoryPath) && !Directory.Exists(databaseDirectoryPath))
+				Directory.CreateDirectory(databaseDirectoryPath);
+
+			options.UseSqlite(connectionString);
+		});
+
+		serviceCollection.AddScoped<IUnitOfWork>(services =>
+		{
+			return Measure.Action("Creating UnitOfWork", () =>
+			{
+				CaveOfWondersDbContext dbContext = services.GetRequiredService<CaveOfWondersDbContext>();
+				dbContext.Database.EnsureCreated();
+				return new SQLiteUnitOfWork(dbContext);
+			});
+		});
+	}
+}
+
+internal static class Measure
+{
+	public static void Action(string actionName, Action action)
+	{
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		action();
+		stopwatch.Stop();
+
+		Console.WriteLine($"{actionName}: {stopwatch.ElapsedMilliseconds} ms.");
+	}
+
+	public static T Action<T>(string actionName, Func<T> action)
+	{
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		T result = action();
+		stopwatch.Stop();
+
+		Console.WriteLine($"{actionName}: {stopwatch.ElapsedMilliseconds} ms.");
+
+		return result;
+	}
 }
